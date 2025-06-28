9aff817b2d38e09da9adfca9f6c9cbfb522e5622
aziz-creator-dev <aziz@covilla.tn>
2025-06-28 10:18:05 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() async {
 
   await Supabase.initialize(
     url: 'https://oonizclzxmtfxitektwy.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbml6Y2x6eG10ZnhpdGVrdHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NTA4NjcsImV4cCI6MjA2NTQyNjg2N30.97y30jbU1SsV7-hXgnXVRNUVq6-rOXpe_bbeUd2Gow0',
   );
 
   runApp(const AgriGestApp());
